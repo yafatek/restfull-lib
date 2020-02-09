ca1582606452012982f07f99ef9bf3ad2395ee24
Feras <feras.alawadi@zarcony.com>
2020-02-09 11:31:44 +0300

    Feras: #21 adding support for annotations

---
+++ src/main/java/dev/yafatek/annotations/ApiAnnotations.java
@@ -0,0 +1,1 @@
+package dev.yafatek.annotations;import java.lang.annotation.ElementType;import java.lang.annotation.Retention;import java.lang.annotation.RetentionPolicy;import java.lang.annotation.Target;@Retention(RetentionPolicy.RUNTIME)@Target(ElementType.METHOD)public @interface ApiAnnotations {	String apiUrl();}
