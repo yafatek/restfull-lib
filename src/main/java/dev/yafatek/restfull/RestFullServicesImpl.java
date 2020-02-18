0682f4248ac22d938c1364a289fda535a1a93c9c
Feras <feras.alawadi@zarcony.com>
2020-02-18 16:01:47 +0300

    MicroService implementation

---
+++ src/main/java/dev/yafatek/restfull/RestFullServicesImpl.java
@@ -0,0 +1,1 @@
+package dev.yafatek.restfull;import java.util.List;public final class RestFullServicesImpl extends RestFullServices<String> {	// static methods only.	private RestFullServicesImpl() {	}	@Override	public List<String> indexAll() {		return super.indexAll();	}}
